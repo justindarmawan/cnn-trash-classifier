00e8731bdadadba2d2329f2f1fcbe92ddc7cbb6a
justin.darmawan <jdarmawan.jd@gmail.com>
2024-12-01 21:04:47 +0700

    adjust transformation & layers

--- train.py
+++ train.py
@@ -6,7 +6,7 @@ from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
 from tensorflow.keras.preprocessing.image import load_img, img_to_array
-from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.optimizers import Nadam
 from tensorflow.keras.callbacks import LambdaCallback
 from tensorflow.keras.layers import BatchNormalization
 from sklearn.model_selection import train_test_split
@@ -57,15 +57,13 @@ def main():
 
     train_datagen = ImageDataGenerator(
         rescale=1.0/255,
-        rotation_range=40,
-        width_shift_range=0.3,
-        height_shift_range=0.3,
-        shear_range=0.3,
-        zoom_range=0.3,
+        rotation_range=20,
+        width_shift_range=0.2,
+        height_shift_range=0.2,
+        shear_range=0.2,
+        zoom_range=0.2,
         horizontal_flip=True,
-        brightness_range=[0.8, 1.2],
-        channel_shift_range=50.0,
-        validation_split=0.2
+        validation_split=0.2 
     )
 
     train_generator = train_datagen.flow_from_directory(
@@ -94,17 +92,14 @@ def main():
         Conv2D(128, (3, 3), activation='relu'),
         BatchNormalization(),
         MaxPooling2D(pool_size=(2, 2)),
-        Conv2D(256, (3, 3), activation='relu'),
-        BatchNormalization(),
-        MaxPooling2D(pool_size=(2, 2)),
         Flatten(),
         Dense(128, activation='relu'),
-        Dropout(0.5),
-        Dense(train_generator.num_classes, activation='softmax')
+        Dropout(0.3),
+        Dense(train_generator.num_classes, activation='softmax') 
     ])
 
     model.compile(
-        optimizer=Adam(learning_rate=lr),
+        optimizer=Nadam(learning_rate=lr),
         loss='categorical_crossentropy',
         metrics=['accuracy']
     )
